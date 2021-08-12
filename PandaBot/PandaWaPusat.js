740691daa8a97d76a325fa5823be1a97f789e85e
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-08-13 01:01:22 +0700

    Update PandaWaPusat.js

--- PandaBot/PandaWaPusat.js
+++ PandaBot/PandaWaPusat.js
@@ -13,7 +13,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 
-DATABASE_URL = process.env.DATABASE_URL === undefined ? './whatsasena.db' : process.env.DATABASE_URL;
+DATABASE_URL = process.env.DATABASE_URL === undefined ? 'PandaBot/PandaUserbot.db' : process.env.DATABASE_URL;
 DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBUG);
 
 module.exports = {
